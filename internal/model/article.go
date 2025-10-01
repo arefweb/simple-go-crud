a326a46a6f02b5cc46e99ad978531769fdd8e05e
Aref Movahedzadeh <a.movahedzadeh@mabnadp.com>
2025-10-01 14:27:32 +0330

    tidy the code

--- internal/model/article.go
+++ internal/model/article.go
@@ -4,10 +4,10 @@ import "time"
 
 // Article represents a simple article record.
 type Article struct {
-    ID          int64      `json:"id"`
-    Title       string     `json:"title"`
-    Content     string     `json:"content"`
-    Author      string     `json:"author"`
-    PublishedAt *time.Time `json:"published_at,omitempty"`
-    CreatedAt   time.Time  `json:"created_at"`
+	ID          int64      `json:"id"`
+	Title       string     `json:"title"`
+	Content     string     `json:"content"`
+	Author      string     `json:"author"`
+	PublishedAt *time.Time `json:"published_at,omitempty"`
+	CreatedAt   time.Time  `json:"created_at"`
 }
